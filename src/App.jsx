8afe1039fe4d3aba51ab66448fc0688a4d3681a9
Pranav Patel <pranav742001@gmail.com>
2026-03-19 13:26:11 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Hello React!</h1>
+      <p>My first React app is running on Mac!</p>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
